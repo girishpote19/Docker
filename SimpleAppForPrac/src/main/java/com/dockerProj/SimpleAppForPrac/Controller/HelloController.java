176d443e4aad3f1c6e2485965810d5dbbe951d36
girish pote <potegirish6@gmail.com>
2025-07-18 06:20:36 +0530

    updated controller

--- SimpleAppForPrac/src/main/java/com/dockerProj/SimpleAppForPrac/Controller/HelloController.java
+++ SimpleAppForPrac/src/main/java/com/dockerProj/SimpleAppForPrac/Controller/HelloController.java
@@ -9,8 +9,9 @@ public class HelloController {
 	public String hello() {
 		return """
                 <html>
-                <body style='font-family: Arial, sans-serif;'>
+                <body style='font-family: Arial, sans-serif; text-align: center;'>
                     <h2>Deployed My First Spring Boot Application on AWS EC2!</h2>
+                    <img src='/images/girishSample.jpg' alt='Girish Photo' width='200' style='border-radius: 50%; margin: 10px;' />
                     <p>Hello! I’m Girish — currently learning DevOps. This is my first Java application successfully deployed on an AWS EC2 instance. 🚀</p>
                     <p>I’m excited to share my progress and connect with others who are also learning DevOps. Let’s grow and learn together!</p>
                     <p>
